4aac169ce431182535fa90419e4057297d553903
Mobin Samani <mobin.samaninasab@gmail.com>
2022-09-16 23:06:23 +0430

    fix: App Title Responsive

--- dist/color-picker.es.js
+++ dist/color-picker.es.js
@@ -1,4 +1,4 @@
-import { openBlock as n, createElementBlock as a, renderSlot as g, createElementVNode as e, toDisplayString as u, normalizeStyle as p, withDirectives as f, Fragment as b, renderList as h, createCommentVNode as _, vModelText as x, vShow as y, resolveComponent as v, createVNode as w, createStaticVNode as k } from "vue";
+import { openBlock as d, createElementBlock as a, renderSlot as g, createElementVNode as e, toDisplayString as p, normalizeStyle as u, withDirectives as f, Fragment as b, renderList as h, createCommentVNode as _, vModelText as x, vShow as y, resolveComponent as v, createVNode as w, createStaticVNode as k } from "vue";
 const C = {
   props: ["modelValue"],
   computed: {
@@ -62,28 +62,28 @@ const C = {
   key: 0,
   class: "flex flex-wrap gap-2"
 }, M = ["title", "onClick"], P = { class: "flex items-center gap-x-4" }, z = /* @__PURE__ */ e("div", { class: "text-gray-700 text-sm" }, "Custom Color:", -1);
-function B(o, l, s, c, r, d) {
-  return n(), a("div", V, [
+function B(o, l, s, c, r, n) {
+  return d(), a("div", V, [
     g(o.$slots, "label", {}, () => [
-      e("div", $, u(s.label) + ":", 1)
+      e("div", $, p(s.label) + ":", 1)
     ]),
     e("div", N, [
       e("div", A, [
         e("button", {
           class: "rounded flex items-center justify-center p-1 cursor-pointer w-full",
-          style: p(`background-color: ${o.model}`),
-          onClick: l[0] || (l[0] = (...t) => d.toggleShow && d.toggleShow(...t))
+          style: u(`background-color: ${o.model}`),
+          onClick: l[0] || (l[0] = (...t) => n.toggleShow && n.toggleShow(...t))
         }, [
-          e("div", E, u(o.model), 1)
+          e("div", E, p(o.model), 1)
         ], 4),
         f(e("div", j, [
-          s.colors && s.colors.length ? (n(), a("div", F, [
-            (n(!0), a(b, null, h(s.colors, (t) => (n(), a("div", {
+          s.colors && s.colors.length ? (d(), a("div", F, [
+            (d(!0), a(b, null, h(s.colors, (t) => (d(), a("div", {
               key: t,
               class: "w-[33px] h-[33px] rounded-lg cursor-pointer",
               title: t,
-              style: p(`background-color: ${t}`),
-              onClick: (i) => d.setColor(t)
+              style: u(`background-color: ${t}`),
+              onClick: (i) => n.setColor(t)
             }, null, 12, M))), 128))
           ])) : _("", !0),
           e("div", P, [
@@ -92,7 +92,7 @@ function B(o, l, s, c, r, d) {
               type: "color",
               class: "grow rounded overflow-hidden",
               "onUpdate:modelValue": l[1] || (l[1] = (t) => o.model = t),
-              onChange: l[2] || (l[2] = (...t) => d.toggleShow && d.toggleShow(...t))
+              onChange: l[2] || (l[2] = (...t) => n.toggleShow && n.toggleShow(...t))
             }, null, 544), [
               [x, o.model]
             ])
@@ -114,7 +114,7 @@ const D = /* @__PURE__ */ m(S, [["render", B]]), L = {
       color: "#8b5cf6"
     };
   }
-}, U = { class: "min-h-screen bg-gray-100 flex items-center justify-center p-8" }, O = { class: "flex flex-col gap-y-8 w-full lg:w-1/2 xl:w-1/3 2xl:w-1/4" }, T = /* @__PURE__ */ e("div", { class: "flex items-center justify-center gap-x-2" }, [
+}, U = { class: "min-h-screen bg-gray-100 flex items-center justify-center p-8" }, O = { class: "flex flex-col gap-y-8 w-full lg:w-1/2 xl:w-1/3 2xl:w-1/4" }, T = /* @__PURE__ */ e("div", { class: "flex items-center justify-center gap-2 flex-wrap text-center" }, [
   /* @__PURE__ */ e("div", { class: "text-gray-600" }, "A Vue Color Picker Component by"),
   /* @__PURE__ */ e("a", {
     class: "font-bold",
@@ -125,9 +125,9 @@ const D = /* @__PURE__ */ m(S, [["render", B]]), L = {
   disabled: "",
   class: "bg-blue-900 text-white rounded p-2 opacity-25"
 }, "Submit", -1);
-function J(o, l, s, c, r, d) {
+function J(o, l, s, c, r, n) {
   const t = v("color-picker");
-  return n(), a("div", U, [
+  return d(), a("div", U, [
     e("div", O, [
       T,
       e("div", q, [
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen bg-gray-100 flex items-center justify-center p-8">
     <div class="flex flex-col gap-y-8 w-full lg:w-1/2 xl:w-1/3 2xl:w-1/4">
-      <div class="flex items-center justify-center gap-x-2">
+      <div class="flex items-center justify-center gap-2 flex-wrap text-center">
         <div class="text-gray-600">A Vue Color Picker Component by</div>
 
         <a class="font-bold" href="https://MobinSamani.com" target="_blank">MobinSamani.com</a>
