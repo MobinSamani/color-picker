cb60538350d70a9e8a58018422236b9d3b68a001
Mobin Samani <mobin.samaninasab@gmail.com>
2022-09-16 20:20:48 +0430

    feat: Color Picker Added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
-    <div class="bg-white shadow rounded-lg p-8">
-      <color-picker />
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center p-8">
+    <div class="flex flex-col gap-y-8 w-full lg:w-1/2 xl:w-1/3 2xl:w-1/4">
+      <div class="flex items-center justify-center gap-x-2">
+        <div class="text-gray-600">A Vue Color Picker Component by</div>
+
+        <a class="font-bold" href="https://MobinSamani.com" target="_blank">MobinSamani.com</a>
+      </div>
+
+      <div class="bg-white shadow rounded-lg p-8">
+        <div class="flex flex-col gap-y-8">
+          <div class="flex flex-col gap-y-4 opacity-25">
+            <label for="firstName" class="block text-gray-700 text-sm font-bold">First Name</label>
+
+            <input id="firstName" disabled class="rounded border border-gray-300 p-2 outline-none" type="text" placeholder="Edward" />
+          </div>
+
+          <div class="flex flex-col gap-y-4 opacity-25">
+            <label for="lastName" class="block text-gray-700 text-sm font-bold">Last Name</label>
+
+            <input id="lastName" disabled class="rounded border border-gray-300 p-2 outline-none" type="text" placeholder="Edward" />
+          </div>
+
+          <color-picker label="Favorite Color" v-model="color" />
+
+          <button disabled type="submit" class="bg-blue-900 text-white rounded p-2 opacity-25">Submit</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,6 +37,11 @@ export default {
   name: "App",
   components: {
     ColorPicker
+  },
+  data() {
+    return {
+      color: "#8b5cf6"
+    };
   }
 };
 </script>
--- src/components/color-picker.vue
+++ src/components/color-picker.vue
@@ -1,9 +1,84 @@
 <template>
-  <div>A Vue Color Picker Component as Code Example</div>
+  <div class="flex flex-col gap-y-4">
+    <slot name="label">
+      <div class="block text-gray-700 text-sm font-bold">{{ label }}:</div>
+    </slot>
+
+    <div class="relative">
+      <div class="rounded border border-gray-300 p-1">
+        <button class="rounded flex items-center justify-center p-1 cursor-pointer w-full" :style="`background-color: ${model}`" @click="toggleShow">
+          <div class="bg-white rounded-lg text-sm py-1 px-2">
+            {{ model }}
+          </div>
+        </button>
+
+        <div v-show="show" class="bg-white shadow-lg rounded p-4 absolute inset-x-0 top-[53px] z-10 flex flex-col gap-y-4">
+          <div v-if="colors && colors.length" class="flex flex-wrap gap-2">
+            <div
+              v-for="color in colors"
+              :key="color"
+              class="w-[33px] h-[33px] rounded-lg cursor-pointer"
+              :title="color"
+              :style="`background-color: ${color}`"
+              @click="setColor(color)"
+            ></div>
+          </div>
+
+          <div class="flex items-center gap-x-4">
+            <div class="text-gray-700 text-sm">Custom Color:</div>
+            <input :id="name" type="color" class="grow rounded overflow-hidden" v-model="model" @change="toggleShow" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import vModel from "../mixins/vModel.vue";
+
 export default {
-  name: "Color Picker"
+  name: "Color Picker",
+  mixins: [vModel],
+  props: {
+    label: { type: String, default: "Color" },
+    colors: {
+      type: Array,
+      default: () => [
+        "#64748b",
+        "#ef4444",
+        "#f97316",
+        "#f59e0b",
+        "#eab308",
+        "#84cc16",
+        "#22c55e",
+        "#10b981",
+        "#14b8a6",
+        "#06b6d4",
+        "#0ea5e9",
+        "#3b82f6",
+        "#6366f1",
+        "#8b5cf6",
+        "#a855f7",
+        "#d946ef",
+        "#ec4899",
+        "#f43f5e"
+      ]
+    }
+  },
+  data() {
+    return {
+      show: false
+    };
+  },
+  methods: {
+    toggleShow() {
+      this.show = !this.show;
+    },
+    setColor(value) {
+      this.model = value;
+      this.toggleShow();
+    }
+  }
 };
 </script>
